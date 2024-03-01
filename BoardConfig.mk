5d4c62e005e1cb2c949a95754730d097af809c64
Lost-Entrepreneur439 <78825510+Lost-Entrepreneur439@users.noreply.github.com>
2024-02-29 22:30:48 -0330

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,28 +1,12 @@
-#
-# Copyright (C) 2019 The LineageOS Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
-# inherit from common
--include device/samsung/universal8895-common/BoardConfigCommon.mk
 
-DEVICE_PATH := device/samsung/dreamlte
+# Inherit from samsung msm8998-common
+-include device/samsung/msm8998-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/samsung/dreamqlte
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := dreamlte
+TARGET_OTA_ASSERT_DEVICE := dreamqlte
 
 # Kernel
-TARGET_KERNEL_CONFIG := exynos8895-dreamlte_defconfig
-
-# inherit from the proprietary version
--include vendor/samsung/dreamlte/BoardConfigVendor.mk
+TARGET_KERNEL_CONFIG := dreamqlte_chn_open_defconfig
