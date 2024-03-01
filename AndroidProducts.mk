2e8b840109cf98bc73caeacf495c9523f357c48f
Lost-Entrepreneur439 <78825510+Lost-Entrepreneur439@users.noreply.github.com>
2024-02-29 22:27:18 -0330

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_dreamlte.mk
+    $(LOCAL_DIR)/lineage_dreamqlte.mk
+
+COMMON_LUNCH_CHOICES := \
+     lineage_dreamqlte-userdebug \
+     lineage_dreamqlte-eng \
+     lineage_dreamqlte-user
